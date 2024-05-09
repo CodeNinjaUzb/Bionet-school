79ca58f1ab3ba5b943f0a909d0baa5453a5a212f
CodeNinjaUzb <codeninjauzb@gmail.com>
2024-05-10 02:40:41 +0500

    add pupil image was created

---
+++ src/components/AddPupilImage.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import '../styles/editPupil.css'
+import axios from 'axios';
+
+function AddPupilImage({setAddPupilImage , info}) {
+
+      const [file , setFile] = useState([])
+
+      function handle(e) {
+            let uploadedFile = e.target.files[0]
+            setFile(uploadedFile)
+            console.log(file);
+      }
+
+
+
+      const addPupilImage = () => {
+
+            let formdata = new FormData()
+
+            formdata.append('id' , info.id)
+            formdata.append('image' , file)
+
+            axios.post('http://95.46.96.110:8000/api/v1/addpupillist/1/', {formdata}, {
+                  headers: {
+                        'username': 'user@API',
+                        'password': 'APIUser@API'
+                  }
+            }).then(data => {console.log(
+                  data
+            );})
+            .catch(err => {console.log(err);})
+      }
+
+      return (
+            <div className='edit-pupil position-absolute d-flex align-items-center justify-content-center'>
+                  <div className="edit-modal bg-white p-3 rounded-1">
+                        <div className="edit-modal-header d-flex justify-content-between align-items-center">
+                              <div className="edit-pupil-title">
+                                    <h5 className=''>O'quvchi rasmini qo'shish</h5>
+                              </div>
+                              <div className="close">
+                                    <i onClick={() => setAddPupilImage(false)} className="fa-solid fa-rectangle-xmark text-danger"></i>
+                              </div>
+                        </div>
+                        <div className="edit-pupil-body">
+                              <div className="row">
+                                    <div className="col-6">
+                                          <input onChange={(e)=>handle(e)} type="file" name="" id="" />
+                                    </div>
+                                    <div className="col-6"></div>
+                              </div>
+                              <div className="row pt-4">
+                                    <div className="col-6 d-flex flex-column gap-1">
+                                          <button onClick={() => setAddPupilImage(false)} className="btn btn-danger rounded-1 pt-1 pb-1 ps-3 pe-3">Bekor qilish</button>
+                                    </div>
+                                    <div className="col-6 d-flex flex-column gap-1">
+                                          <button onClick={()=>addPupilImage()} className="btn btn-primary rounded-1 pt-1 pb-1 ps-3 pe-3">Saqlash</button>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      );
+}
+
+export default AddPupilImage;
--- src/pages/Pupils.jsx
+++ src/pages/Pupils.jsx
@@ -7,15 +7,17 @@ import EditPupil from '../components/EditPupil';
 import AddPupil from '../components/AddPupil';
 import DeletePupil from '../components/DeletePupil';
 import AddPupilList from '../components/AddPupilList';
+import AddPupilImage from '../components/AddPupilImage';
 
 function Pupils() {
 
       const [pupil, setPupil] = useState([])
-      const [editPupil , setEditPupil] = useState(false)
-      const [addPupil , setAddPupil] = useState(false)
-      const [addPupilList , setAddPupilList] = useState(false)
-      const [deletePupil , setDeletePupil] = useState(false)
-      const [pupilInfo , setPupilInfo] = useState([])
+      const [editPupil, setEditPupil] = useState(false)
+      const [addPupil, setAddPupil] = useState(false)
+      const [addPupilList, setAddPupilList] = useState(false)
+      const [addPupilImage, setAddPupilImage] = useState(false)
+      const [deletePupil, setDeletePupil] = useState(false)
+      const [pupilInfo, setPupilInfo] = useState([])
 
       useEffect(() => {
             axios.get('http://95.46.96.110:8000/api/v1/getpupil/1', {
@@ -35,15 +37,20 @@ function Pupils() {
             setDeletePupil(true)
             setPupilInfo(item)
       }
+      const addImage = (item) => {
+            setAddPupilImage(true)
+            setPupilInfo(item)
+      }
 
 
 
       return (
             <>
-            {editPupil ? <EditPupil setEditPupil={setEditPupil} info={pupilInfo} /> : ''}
-            {addPupil ? <AddPupil setAddPupil={setAddPupil} /> : ''}
-            {addPupilList ? <AddPupilList setAddPupilList={setAddPupilList} /> : ''}
-            {deletePupil ? <DeletePupil setDeletePupil={setDeletePupil} info={pupilInfo} /> : ''}
+                  {editPupil ? <EditPupil setEditPupil={setEditPupil} info={pupilInfo} /> : ''}
+                  {addPupil ? <AddPupil setAddPupil={setAddPupil} /> : ''}
+                  {addPupilList ? <AddPupilList setAddPupilList={setAddPupilList} /> : ''}
+                  {addPupilImage ? <AddPupilImage setAddPupilImage={setAddPupilImage} info={pupilInfo} /> : ''}
+                  {deletePupil ? <DeletePupil setDeletePupil={setDeletePupil} info={pupilInfo} /> : ''}
                   <div className='main d-flex w-100'>
                         <div className="left">
                               <Sidebar />
@@ -53,14 +60,14 @@ function Pupils() {
                               <div className="pupils p-5">
                                     <div className="row">
                                           <div className="col-4">
-                                                <button onClick={()=>setAddPupilList(true)} className="btn btn-primary ps-3 pe-3 pt-1 pb-1 rounded-0 d-flex align-items-center gap-2">
+                                                <button onClick={() => setAddPupilList(true)} className="btn btn-primary ps-3 pe-3 pt-1 pb-1 rounded-0 d-flex align-items-center gap-2">
                                                       <i className="fa-solid fa-plus"></i>
                                                       <p className="m-0">O'quvchilar roýxatini qo'shish</p>
                                                 </button>
                                           </div>
                                           <div className="col-4"></div>
                                           <div className="col-4 d-flex justify-content-end">
-                                                <button onClick={()=>setAddPupil(true)} className="btn btn-primary ps-3 pe-3 pt-1 pb-1 rounded-0 d-flex align-items-center gap-2">
+                                                <button onClick={() => setAddPupil(true)} className="btn btn-primary ps-3 pe-3 pt-1 pb-1 rounded-0 d-flex align-items-center gap-2">
                                                       <i className="fa-solid fa-plus"></i>
                                                       <p className="m-0">O'quvchi qo'shish</p>
                                                 </button>
@@ -70,6 +77,7 @@ function Pupils() {
                                           <div className="col-12">
                                                 <div className="pupil-table pt-4">
                                                       <div className="pupil-table-head d-grid bg-primary pt-1 pb-1 text-white">
+                                                            <div className="head-item m-0 fw-bold text-center">Rasm</div>
                                                             <p className="head-item m-0 fw-bold text-center">Id</p>
                                                             <p className="head-item m-0 fw-bold text-center">Ism-Familiya</p>
                                                             <p className="head-item m-0 fw-bold text-center">Tugilgan sana</p>
@@ -81,14 +89,18 @@ function Pupils() {
                                                             {pupil.map((item) => {
                                                                   return (
                                                                         <div className="pupil-table-body-row d-grid border pt-2 pb-2 ">
+                                                                              <div className="row-item d-flex align-items-center justify-content-center">
+                                                                                    <img className="pupil-img m-0" src={item.image} alt="" />
+                                                                              </div>
                                                                               <p className="row-item m-0 fw-bold text-center">{item.id}</p>
                                                                               <p className="row-item m-0 fw-bold text-center">{item.name}</p>
                                                                               <p className="row-item m-0 fw-bold text-center">{item.tug_sana}</p>
                                                                               <p className="row-item m-0 fw-bold text-center">{item.phone}</p>
                                                                               <p className="row-item m-0 fw-bold text-center">{item.address}</p>
                                                                               <div className="row-item m-0 fw-bold text-center d-flex align-items-center gap-3 justify-content-center">
-                                                                                    <i onClick={()=>{editHandle(item)}} className="fa-solid fa-pen-to-square text-warning"></i>
-                                                                                    <i onClick={()=>deleteHandle(item)} className="fa-solid fa-trash text-danger"></i>
+                                                                                    <i onClick={() => { editHandle(item) }} className="fa-solid fa-pen-to-square text-warning"></i>
+                                                                                    <i onClick={() => deleteHandle(item)} className="fa-solid fa-trash text-danger"></i>
+                                                                                    <i onClick={()=> addImage(item)} className="fa-solid fa-image text-primary"></i>
                                                                               </div>
                                                                         </div>
                                                                   )
